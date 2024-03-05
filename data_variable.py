3df4d2bf969dfa07f45e2a63916be0e515aeb54c
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-05 10:24:00 +0530

    data_variable.py

---
+++ data_variable.py
@@ -0,0 +1,19 @@
+# importing library
+import sys
+# decelaration
+x = None
+# initialization
+x = 10
+# Assignment
+print(x)
+x = 20
+
+# usage
+result = x * 2
+print(x)
+# Updating
+x = x + 5
+
+print(x)
+print(result)
+print(sys.getsizeof(result)
