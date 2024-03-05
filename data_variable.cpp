f362952f59544c6b314d6894ef050e17924f31b3
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-05 10:31:11 +0530

    data_variable.cpp

---
+++ data_variable.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+  //declaration
+  int x;
+// Initialization
+x=10;
+// assignment
+x=20;
+
+// Usage
+int result =x*2;
+// Updating
+x=x+5;
+
+cout<<(x)<<endl;
+cout<<(sizeof(x));
+}
