844eafd28749ffdf82e4ee7e6b07d82f8975e9b3
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-05 10:11:05 +0530

    data_variable.java

---
+++ data_variable.java
@@ -0,0 +1,19 @@
+class data_variable{
+  public static void main(String[] args){
+    
+// declaration
+    int x;
+// initialization
+  x =10;
+  // Assignment
+    x=20;
+    //Usage
+int result = x*2;
+    // updating
+    x=x+5;
+    // Scope
+    
+  System.out.println(result);
+   
+}
+}
