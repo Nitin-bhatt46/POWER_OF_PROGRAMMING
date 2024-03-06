0084d62176cda6627acc72f01262d08ff1492178
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-06 10:40:15 +0530

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter any number: " << endl;
+    cin >> a;
+    cout << "The number is " << a << endl;
+    return 0;
+}
