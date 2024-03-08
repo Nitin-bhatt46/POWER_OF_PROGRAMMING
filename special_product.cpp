05f4fb8319201852399a89fc363b4578035b2be4
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-08 09:50:01 +0530

    special_product.cpp

---
+++ special_product.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 6;
+    int b= 3;
+    cout<<"value of a = "<<a<<endl;
+    cout<<"value of b = "<<b<<endl;
+    cout<<"product of a and b = "<<a<<"*"<<b;
+}
