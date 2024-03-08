825313035d448199aa9255c82c95a755f99ad651
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-08 09:58:58 +0530

    product.java

---
+++ product.java
@@ -0,0 +1,15 @@
+class product{
+    
+    public static void main(String[] args){
+        
+        int a =10;
+        int b = 8;
+        System.out.println("value of a = "+ a);
+        System.out.println("value of b = "+ b);
+        System.out.println("product of a and b = "+ a*b);
+        
+    }
+    
+    
+    
+}
