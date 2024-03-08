8af10dcd97e3aba17e32bd3cb5c50fec81c5e2de
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-08 10:00:33 +0530

    substraction.java

---
+++ substraction.java
@@ -0,0 +1,13 @@
+class substraction{
+    
+    public static void main(String[] args){
+        
+        int a =10;
+        int b = 8;
+        System.out.println("value of a = "+ a);
+        System.out.println("value of b = "+ b);
+        int c = a-b;
+        System.out.println("substraction of a - b = "+ c);
+        }
+    
+}
