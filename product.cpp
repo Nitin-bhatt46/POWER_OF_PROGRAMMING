78367afccbf0f92a82bb70e56dae582f3c80852a
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-08 09:46:41 +0530

    product.cpp

---
+++ product.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =5;
+    int b=6;
+    cout<<"value of a = "<<a<<endl;
+    cout<<"value of b = "<<b<<endl;
+    cout<<"product of a and b "<<a*b;
+}
