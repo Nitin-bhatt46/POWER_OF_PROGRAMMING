aa9085e952400578973d0d2342abd9da402d70c3
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-08 10:02:35 +0530

    Special_product.java

---
+++ Special_product.java
@@ -0,0 +1,13 @@
+class Special_product{
+    
+    public static void main(String[] args){
+        
+        int a =6;
+        int b = 3;
+        System.out.println("value of a = "+ a);
+        System.out.println("value of b = "+ b);
+        int c = a-b;
+        System.out.println("product of a * b = "+ a  +"*"+b);
+        }
+    
+}
