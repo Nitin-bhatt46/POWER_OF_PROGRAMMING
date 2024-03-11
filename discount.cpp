412e486ca3a58f76989cf621b3327a9546ca7fa1
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-11 10:03:19 +0530

    discount.cpp

---
+++ discount.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int age;
+ 
+ cout<<"enter your age to find out discount eligibility = ";
+ cin>>age;
+ 
+ if(age>65 || age<12){
+     cout<<"Yes";
+ }
+ else{
+     cout<<"No";
+ }
+    }
