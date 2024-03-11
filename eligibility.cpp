7ee2f81bf6fdca5cb89ce05059b3e6c3344864f7
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-11 09:10:52 +0530

    eligibility.cpp

---
+++ eligibility.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+ 
+        int age = 15;
+  
+        if(age>18){
+         cout<<"Adult";
+        }
+        else{
+            cout<<"Teenager";
+        }
+    }
