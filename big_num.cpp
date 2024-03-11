a8bf8472b45a529ee041b1db16a8358123aa21c5
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-11 09:35:29 +0530

    big_num.cpp

---
+++ big_num.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+ 
+        int a =10 ;
+        int b=15;
+        if(a>b){
+           cout<<("a is greater than b.");
+        }
+        
+        else {
+             cout<<("b is greater than a.");
+        }
+    }
