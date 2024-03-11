d7161d888326222b9f1b6f380cc5a46bcc4cbebe
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-11 09:10:16 +0530

    eligibility.java

---
+++ eligibility.java
@@ -0,0 +1,12 @@
+class eligibility {
+    public static void main(String[] args) {
+        int age = 15;
+  
+        if(age>18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Teenager");
+        }
+    }
+}
