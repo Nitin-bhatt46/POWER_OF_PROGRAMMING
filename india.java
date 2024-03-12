5b6b99b9efce63369b79ea5917f0537bba4c9691
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-12 09:03:37 +0530

    india.java

---
+++ india.java
@@ -0,0 +1,8 @@
+class india {
+    public static void main(String[] args)
+  {
+        for(int i=0;i<20;i++){
+        System.out.println("India will win the World Cup 2023"); 
+                             }
+    }
+          }
