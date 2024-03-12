e2a8b6e24538ac42e3d5bf9b3407780013138b00
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-12 09:43:10 +0530

    table4.cpp

---
+++ table4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n=4;
+    
+    for(int i=0;i<=10;i++){
+        cout<<n<<" X "<<i<<" = "<<n*i<<endl;
+    }
+    
+
+    return 0;
+}
