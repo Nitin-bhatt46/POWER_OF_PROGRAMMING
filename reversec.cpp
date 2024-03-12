00b3df51851d3842d5e12fc8311bfa4e066b936c
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-12 10:17:19 +0530

    reversec.cpp

---
+++ reversec.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter the from where you want to start the loop = ";
+    cin>>n;
+    for(int i=n;i>=0;i--){
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
