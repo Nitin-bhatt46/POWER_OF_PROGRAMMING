acaa6edecff86b88414405657d626edce252c48d
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-12 10:20:43 +0530

    reversec.java

---
+++ reversec.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class reversec {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("enter the number from where the loop will start = ");
+      int n = sc.nextInt();
+      
+        for(int i=n;i>=0;i--){
+            System.out.println(i);
+          
+    }
+    }
+}
