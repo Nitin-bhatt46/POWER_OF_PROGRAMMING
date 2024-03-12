96dbd809e025ca634b477930cb2cc820ea1039d1
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-12 09:47:20 +0530

    table4.java

---
+++ table4.java
@@ -0,0 +1,12 @@
+class table4 {
+    public static void main(String[] args) {
+       
+        int n =4;
+    
+      
+        for(int i=0;i<=10;i++){
+            System.out.println(n + " X " + i +" = "+ n*i);
+          
+    }
+    }
+}
