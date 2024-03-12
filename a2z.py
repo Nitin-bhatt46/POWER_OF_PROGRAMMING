fb48d5c174624798b55866c9c45c64f4d046683c
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-12 10:00:49 +0530

    Create a2z.py

---
+++ a2z.py
@@ -0,0 +1,6 @@
+for char in range(ord('a'), ord('z') + 1):
+    # ord  provide unicode the character. 
+    print(chr(char))
+    # chr covert unicode into character.
+# to print all in one line we use
+   # print(chr(char), end = '  ')
