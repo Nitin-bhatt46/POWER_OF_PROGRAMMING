1694ca8388853d846b9b1b2d53726acfd6f97429
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-13 08:57:42 +0530

    sequence.java

---
+++ sequence.java
@@ -0,0 +1,10 @@
+class sequence {
+    
+    public static void main(String[] args){
+        
+        for(int i= 280; i>=250;i--){
+            System.out.println(i);
+        }
+    }
+    
+    }
