474e6fc78d1e761b28f7330b3522f8c4fc33bd60
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-13 09:07:34 +0530

    reverse_capital_alphabet.java

---
+++ reverse_capital_alphabet.java
@@ -0,0 +1,10 @@
+class reverse_capital_alphabet {
+    
+    public static void main(String[] args){
+        
+        for(char i= 'Z'; i>='A';i--){
+            System.out.print(i);
+        }
+    }
+    
+    }
