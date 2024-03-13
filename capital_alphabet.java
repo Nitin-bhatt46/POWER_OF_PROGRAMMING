587abbd2a6be593f87c4eabb1f6d124a5ff104fa
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-13 09:00:42 +0530

    capital_alphabet.java

---
+++ capital_alphabet.java
@@ -0,0 +1,10 @@
+class capital_alphabet {
+    
+    public static void main(String[] args){
+        
+        for(char i= 'A'; i<='Z';i++){
+            System.out.print(i);
+        }
+    }
+    
+    }
