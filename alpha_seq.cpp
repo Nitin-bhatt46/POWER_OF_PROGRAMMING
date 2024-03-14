ea5d113f2770a878166cc9e75150dd0f1e2e0d89
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:21:31 +0530

    Create alpha_seq.cpp

---
+++ alpha_seq.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    for(int i =1;i<=5;i++){
+            for(char j ='F';j<='K';j++){
+               cout<<j<<" ";
+            }
+            cout<<""<<endl;
+        }
+    
+    return 0;
+}
