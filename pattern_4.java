14581e3bde9ae09b63de30c0c0aacdcb282a763c
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 09:56:56 +0530

    pattern_4.java

---
+++ pattern_4.java
@@ -0,0 +1,14 @@
+class pattern_4{
+    
+    public static void main(String[] args){
+        for(int i =0;i<5;i++){
+            for(int j=0 ;j<=5;j++)
+            {
+            System.out.print("4 ");
+        }
+        System.out.println();
+    }
+    
+    
+}
+}
