96be87b009864bbffb2edbdbe9d400c1b8d7a2ee
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 11:01:07 +0530

    Create tri_num_2.java

---
+++ tri_num_2.java
@@ -0,0 +1,12 @@
+
+class tri_num_2   {
+    public static void main(String[] args) {
+        for(int i = 10;i<=15;++i){
+            for(int j =10;j<=i;++j){
+              
+                System.out.print(j+" " );
+            }
+            System.out.println("");
+        }
+    }
+}
