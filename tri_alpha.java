4d781ce65765feb784a521c7da83d5d0850934c0
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:48:32 +0530

    Create tri_alpha.java

---
+++ tri_alpha.java
@@ -0,0 +1,12 @@
+
+class tri_alpha   {
+    public static void main(String[] args) {
+        for(char i ='A';i<='E';++i){
+            for(char j ='A';j<=i;++j){
+              
+                System.out.print(j+" " );
+            }
+            System.out.println("");
+        }
+    }
+}
