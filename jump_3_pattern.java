a028f3f9c6d75550b3521fb200cdd07e4ab09044
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:06:06 +0530

    jump_3_pattern.java

---
+++ jump_3_pattern.java
@@ -0,0 +1,11 @@
+
+class jump_3_pattern {
+    public static void main(String[] args) {
+        for(int i =1;i<=6;i++){
+            for(int j =1;j<=5;j++){
+                System.out.print(j*j+" " );
+            }
+            System.out.println("");
+        }
+    }
+}
