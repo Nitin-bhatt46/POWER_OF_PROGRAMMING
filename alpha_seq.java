ead94025c1ff2169834439317aa75f633a1eb13d
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:21:07 +0530

    Create alpha_seq.java

---
+++ alpha_seq.java
@@ -0,0 +1,11 @@
+
+class alpha_seq  {
+    public static void main(String[] args) {
+        for(int i =1;i<=5;i++){
+            for(char j ='F';j<='K';j++){
+                System.out.print(j+" " );
+            }
+            System.out.println("");
+        }
+    }
+}
