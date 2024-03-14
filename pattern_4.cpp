1ae8606bc2c6fb0d1f5fe02cdc8b055b1691b426
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 09:57:31 +0530

    pattern_4.cpp

---
+++ pattern_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    for(int i =0;i<5;i++){
+        for(int j=0;j<6;j++){
+            cout<<"4 ";
+        }
+        cout<<" "<<endl;
+    }
+
+    return 0;
+}
