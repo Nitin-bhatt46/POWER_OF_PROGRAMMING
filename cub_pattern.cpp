a129950bfd32d122d8ee2abbf8a525da9bd99309
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:16:12 +0530

    Create cub_pattern.cpp

---
+++ cub_pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    for(int i =1;i<=5;i++){
+            for(int j =1;j<=6;j++){
+               cout<<j*j*j<<" ";
+            }
+            cout<<""<<endl;
+        }
+    
+    return 0;
+}
