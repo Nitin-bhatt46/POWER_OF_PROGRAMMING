ab1957a6409387b8138d554f23bbebaced22e028
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:15:18 +0530

    Create cub_pattern.java

---
+++ cub_pattern.java
@@ -0,0 +1,10 @@
+class cub_pattern {
+    public static void main(String[] args) {
+        for(int i =1;i<=5;i++){
+            for(int j =1;j<=6;j++){
+                System.out.print(j*j*j+" " );
+            }
+            System.out.println("");
+        }
+    }
+}
