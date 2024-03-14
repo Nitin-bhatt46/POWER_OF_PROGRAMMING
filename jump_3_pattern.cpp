a6d4f59da6aaecd599863538cac957e2589cd9e3
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-14 10:06:37 +0530

    jump_3_pattern.cpp

---
+++ jump_3_pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    for(int i =1;i<=6;i++){
+            for(int j =1;j<=5;j++){
+               cout<<j*j<<" ";
+            }
+            cout<<""<<endl;
+        }
+    
+    return 0;
+}
