ce96b65cc2bf1b130086e293924a474474c33f45
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-18 10:57:08 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,69 @@
+
+a = 5
+b = 3
+
+# Arithmetic operators
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+remainder = a % b
+
+# Bitwise operators
+bitwise_and = a & b
+bitwise_or = a | b
+bitwise_xor = a ^ b
+bitwise_complement_a = ~a
+bitwise_left_shift = a << 1
+bitwise_right_shift = a >> 1
+
+# Unary operators
+unary_minus = -a
+unary_plus = +a
+unary_increment = a + 1
+unary_decrement = b - 1
+
+# Comparison operators
+equal_to = a == b
+not_equal_to = a != b
+greater_than = a > b
+less_than = a < b
+
+# Logical operators
+logical_and = (a < 10) and (b > 1)
+logical_or = (a < 10) or (b < 1)
+logical_not = not (a == 10)
+
+# Display results
+print("Arithmetic Operators:")
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+print("Remainder:", remainder)
+
+print("\nBitwise Operators:")
+print("Bitwise AND:", bitwise_and)
+print("Bitwise OR:", bitwise_or)
+print("Bitwise XOR:", bitwise_xor)
+print("Bitwise Complement of a:", bitwise_complement_a)
+print("Bitwise Left Shift:", bitwise_left_shift)
+print("Bitwise Right Shift:", bitwise_right_shift)
+
+print("\nUnary Operators:")
+print("Unary Minus:", unary_minus)
+print("Unary Plus:", unary_plus)
+print("Unary Increment of a:", unary_increment)
+print("Unary Decrement of b:", unary_decrement)
+
+print("\nComparison Operators:")
+print("Equal To:", equal_to)
+print("Not Equal To:", not_equal_to)
+print("Greater Than:", greater_than)
+print("Less Than:", less_than)
+
+print("\nLogical Operators:")
+print("Logical AND:", logical_and)
+print("Logical OR:", logical_or)
+print("Logical NOT:", logical_not)
+
