dafe370d61ee1340fbe86e9ba222031e53403a89
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 08:51:32 +0530

    Create pos_even.cpp

---
+++ pos_even.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+   cout<<"Enter any number = ";
+   cin>>n;
+   if(n%2==0 && n>=0){
+       cout<<"number is +ve and even also";
+   }
+   else{
+       cout<<"Number is odd or -ve ";
+   }
+    return 0;
+}
