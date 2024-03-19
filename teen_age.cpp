95c79c70706692339b26db57b9990343c0e2be9d
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 08:56:48 +0530

    Create teen_age.cpp

---
+++ teen_age.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+   cout<<"Enter your age  = ";
+   cin>>n;
+   if(n<=19 && n>=13){
+       cout<<"YOu are teenager";
+   }
+   else{
+       cout<<"YOu are not a teenager ";
+   }
+    return 0;
+}
