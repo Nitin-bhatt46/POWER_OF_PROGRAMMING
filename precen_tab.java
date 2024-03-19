aa47733a1ab2c1582c7c08352e228d914b22790b
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 09:29:48 +0530

    Create precen_tab.java

---
+++ precen_tab.java
@@ -0,0 +1,17 @@
+public class precen_tab {
+    public static void main(String[] args) {
+       
+        boolean a, b, c, d;
+        a = 2 * 3 - 48 == 5 / 4 * 6;
+        b = 6 < 2 - 4 * 8 / 2;
+        c = 5 > 4 && 4 < 3 / 2 - 8 % 4 + 5; // Use && for logical AND
+        d = 14 - 8 + 92 > 2 + 70;
+
+        // 0 is false
+        // 1 is true
+        System.out.println(a); // Output: false
+        System.out.println(b); // Output: false
+        System.out.println(c); // Output: false
+        System.out.println(d); // Output: true
+    }
+}
