b7e4c47fc393679f49c6e013d874f6de5a1e3378
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 09:30:17 +0530

    Create precen_tab.cpp

---
+++ precen_tab.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    bool a,b,c,d;
+
+    a = 2*3-48==5/4*6;
+    b = 6<2-4*8/2;
+    c = 5>4<3/2-8%4+5;
+    d = 14-8+92 > 2+70;
+    
+    // 0 is false
+    // 1 is ture
+    
+    cout<< a<<endl;
+    cout<< b<<endl;
+    cout<< c<<endl;
+    cout<< d<<endl;
+    return 0;
+}
