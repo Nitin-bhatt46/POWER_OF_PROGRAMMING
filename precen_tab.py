fdde6901b631998d39ee8b2513e9f873ed32a0aa
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 09:31:29 +0530

    Create precen_tab.py

---
+++ precen_tab.py
@@ -0,0 +1,15 @@
+def main():
+    a = 2 * 3 - 48 == 5 / 4 * 6
+    b = 6 < 2 - 4 * 8 / 2
+    c = 5 > 4 < 3 / 2 - 8 % 4 + 5
+    d = 14 - 8 + 92 > 2 + 70
+    
+    # 0 is false
+    # 1 is true
+    print(a)  # Output: False
+    print(b)  # Output: False
+    print(c)  # Output: False
+    print(d)  # Output: True
+
+if __name__ == "__main__":
+    main()
