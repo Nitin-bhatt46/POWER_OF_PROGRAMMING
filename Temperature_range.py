4af85c50da3e8233dc99cb7864b758fc7890f4e6
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 08:43:23 +0530

    Create Temperature_range.py

---
+++ Temperature_range.py
@@ -0,0 +1,9 @@
+
+t = int(input("enter the temperature of pool in Fahrenheit = "))
+
+if(t<90 and t>70):
+    print("Suitable for swimming");
+   
+else:
+    print("Not suitable for swimming");
+   
