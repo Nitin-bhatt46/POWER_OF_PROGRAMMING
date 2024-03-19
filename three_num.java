b834ff0a03dd398ecfb8f329b8703f370c823618
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-19 09:07:25 +0530

    Create three_num.java

---
+++ three_num.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+class three_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number in a =");
+        int a = sc.nextInt();
+        System.out.println("Enter any number in b = ");
+        int b = sc.nextInt();
+        System.out.println("Enter any number in c = ");
+        int c = sc.nextInt();
+    if(a>b && a>c){
+       System.out.println("Yes");
+   }
+   else{
+       System.out.println("NO");
+   }
+    }
+}
