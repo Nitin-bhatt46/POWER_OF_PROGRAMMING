d0827cd93feee50905f492d4605a4722a05d367f
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-20 11:16:18 +0530

    Create mon_switch.cpp

---
+++ mon_switch.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+ int n;
+ cout<<"enter the number  till 12  = ";
+ cin>>n;
+ 
+ switch(n){
+     case 1:
+     cout<<"Jan";
+     break;
+     case 2:
+     cout<<"FEB";
+     break;
+     case 3:
+     cout<<"MAR";
+     break;
+     case 4:
+     cout<<"APR";
+     break;
+     case 5:
+     cout<<"MAY";
+     break;
+     case 6:
+     cout<<"JUN";
+     break;
+     case 7:
+     cout<<"JULY";
+     break;
+     case 8:
+     cout<<"AUG";
+     break;
+     case 9:
+     cout<<"SEPT";
+     break;
+     case 10:
+     cout<<"OCT";
+     break;
+     case 11:
+     cout<<"NOV";
+     break;
+     case 12:
+     cout<<"DEC";
+     break;
+     default :
+    cout<<"invalid number";
+ }
+ 
+    return 0;
+}
