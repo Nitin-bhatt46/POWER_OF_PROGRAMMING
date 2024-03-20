cc79cef0337cfd529b725f42effdf96ae7fc07cf
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-20 11:15:40 +0530

    Create mon_swtich.java

---
+++ mon_swtich.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class mon_swtich {
+ 
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int sum=1;
+        int n;
+        System.out.print("enter any number till 12 = ");
+        n = sc.nextInt();
+        
+    switch(n){
+        case 1:
+            System.out.println("Jan");
+            break;
+        case 2:
+            System.out.println("feb");
+            break;
+        case 3:
+            System.out.println("MAR");
+            break;
+        case 4:
+            System.out.println("APR");
+            break;
+        case 5:
+            System.out.println("MAY");
+            break;
+        case 6:
+            System.out.println("JUN");
+            break;
+        case 7:
+            System.out.println("JULY");
+            break;
+        case 8:
+            System.out.println("AUG");
+            break;
+        case 9:
+            System.out.println("SEPT");
+            break;
+        case 10:
+            System.out.println("OCT");
+            break;
+        case 11:
+            System.out.println("NOV");
+            break;
+        case 12:
+            System.out.println("DEC");
+            break;
+            
+    }
+            
+ }
+
+}
+    
