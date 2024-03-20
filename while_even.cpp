56e2fcde12fbd4254e75e1cfdd48fad71c35bf5a
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-20 10:38:23 +0530

    Create while_even.cpp

---
+++ while_even.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a= 1;
+ int n;
+ cout<<"enter the number  till u need even number = ";
+ cin>>n;
+ 
+ while(n>a){
+     if(a%2==0){
+         cout<<a<< endl;
+     }
+     a++;
+ }
+    return 0;
+}
