709b0d6ad0af6463c2aec2624924fe3620eb8a37
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-22 09:59:29 +0530

    Create func_cube.py

---
+++ func_cube.py
@@ -0,0 +1,7 @@
+def cube(a):   # creating a function.
+    
+    result = a*a*a
+    return result
+    
+a= int ( input("enter any number to find out its cubic value."))
+print(cube(a)) # calling function.
