15fba140138fda9a13683864f32f3b1429326693
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-22 11:10:49 +0530

    Create print_slogan_func.cpp

---
+++ print_slogan_func.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void print(int a){
+    for( int i =0;i<a;i++){
+        cout<<"Hello Coder Army"<<endl;
+    }
+
+
+}
+int main() {
+    
+    int a;
+
+cout<<"enter the number for slogan print  = ";
+cin>>a;
+
+print(a);
+    return 0;
+}
