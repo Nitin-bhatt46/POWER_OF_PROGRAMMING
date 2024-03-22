e731a63df8ba05f35bf23246fd6c8b6eaa28a776
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-22 11:14:50 +0530

    Create print_slogan_func.py

---
+++ print_slogan_func.py
@@ -0,0 +1,8 @@
+def print_slogan(a):
+    for i in range( 0,a):
+        print("Hello Coder Army ")
+
+    
+
+a = int(input("Enter any number for slogan print "))
+print_slogan(a)  # Calling the function
