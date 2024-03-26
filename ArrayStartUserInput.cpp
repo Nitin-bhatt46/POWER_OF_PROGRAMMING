5a747188f63c5e4b453d03a9d156529b332c9d85
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-26 09:35:38 +0530

    Create ArrayStartUserInput.cpp

---
+++ ArrayStartUserInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // declamation of array.
+  int a[5];
+cout<<"enter the array of size 5 = "<<endl;
+
+  for(int i =0;i<5;i++){
+
+      cin>>a[i];
+  }
+  
+  for(int i = 0 ;i<5;i++){
+      cout<<a[i];
+  }
+  
+    return 0;
+}
