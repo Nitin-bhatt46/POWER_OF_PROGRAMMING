2747255c99a1540697ac806b1b8fd864f55d52cd
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-26 09:24:59 +0530

    Create ArrayStart.java

---
+++ ArrayStart.java
@@ -0,0 +1,13 @@
+
+class ArrayStart.java {
+    public static void main(String[] args) {
+      // decleration of array.
+      // initalisation of array.
+      int[] a={1,2,3,4};
+      // array iteration.
+      System.out.println(a[3]);
+      
+      
+      
+    }
+}
