11211ffc489d80c845255985ebf5856e75cfa4fd
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-03-27 07:26:14 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,16 @@
+# Creating a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Accessing elements
+print(my_tuple[0])  # Output: 1
+print(my_tuple[2])  # Output: 3
+
+# Slicing
+print(my_tuple[1:4])  # Output: (2, 3, 4)
+
+# Immutable nature
+# This will raise an error:
+# my_tuple[0] = 10
+
+# Length of tuple
+print(len(my_tuple))  # Output: 5
