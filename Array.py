22d605448ba3756235c847a1a757a717407852c8
Nitin-bhatt46 <122802161+Nitin-bhatt46@users.noreply.github.com>
2024-04-08 10:24:12 +0530

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,19 @@
+from array import *
+
+# Create an array of integers
+a1 = array('i', [1, 2, 3, 4, 5])
+
+# Create an array of floating-point numbers
+a2 = array('f', [1.1, 2.2, 3.3, 4.4, 5.5])
+
+# Create an array of characters
+a3 = array('u', ['a', 'b', 'c', 'd', 'e'])
+
+# Create an array of bytes
+a4 = array('B', [65, 66, 67, 68, 69])
+
+# Print the arrays
+print("Array of integers:", a1)
+print("Array of floating-point numbers:", a2)
+print("Array of characters:", a3)
+print("Array of bytes:", a4)
